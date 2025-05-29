b55f45665ef21dc2b8c95cf459a8de4c33999cf9
Andrik Rodriguez <jose.martinez@htatec.com>
2025-05-29 11:29:27 -0600

    Update: Agregar la interfaz de product, vista de la informacion en texto simple

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
-function App() {
-    const [count, setCount] = useState(0);
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from '@mui/material';
+import { ProductProvider } from './context/ProductContext';
+import EditProducts from './pages/EditProducts';
+export default function App() {
     return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
+        <ProductProvider>
+            <Router>
+                <Container maxWidth="lg">
+                    <Routes>
+                        <Route path="/" element={<EditProducts />} />
+                    </Routes>
+                </Container>
+            </Router>
+        </ProductProvider>
     );
 }
-
-export default App;
---
+++ src/types/Product.ts
@@ -0,0 +1,21 @@
+/**
+ * Este archivo contiene interfaces y los tipos de datos 
+ * para los productos se envian del api
+ * Porque una interfaz y no una clase?
+ * * - Las interfaces son más ligeras y no generan código adicional en tiempo de ejecución.
+ * * - Las interfaces son ideales para definir la forma de los objetos y no necesitan lógica adicional.
+ */
+
+export interface Product {
+  key_unique: string; // clave única generada aleatoriamente
+  id: string; // id del producto en la base de datos
+  nombre: string;
+  clave: string;
+  unidad_medida: string;
+  precio_sugerido: number;
+  precio_actual: number;
+  inventario_actual: number;
+  categoria?: string; // opcional, ya que no todos los productos tienen categoría
+  inventario_original: number;
+  modificado: boolean;
+}
